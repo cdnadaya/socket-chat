587106270b025ae07764e6307af6dd41e9a4ee09
Cristian <cdnadaya@gmail.com>
2021-02-03 08:55:18 -0300

    segundo commit

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -12,12 +12,15 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -29,29 +32,27 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-//socket.emit('crearMensaje', {
-//    usuario: 'Cristian',
-//  mensaje: 'Hola Mundo'
-//}, function(resp) {
-//  console.log('respuesta server: ', resp);
-//});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//Escuchar cambios cuando un usuario entra o sale del chat
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
-//Mensajes privados
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
 
     console.log('Mensaje Privado:', mensaje);
